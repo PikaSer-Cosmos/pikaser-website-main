3d97ae97fdcd25165000e17326091d2ae418f334
PikachuEXE <pikachuexe@gmail.com>
2022-08-12 09:48:56 +0800

    * Update article hero images to use better placeholders

--- components/content/ContentImg.vue
+++ components/content/ContentImg.vue
@@ -1,9 +1,11 @@
 <template>
-  <NuxtImg :src="src" :alt="alt" :width="width" :height="height" />
+  <NuxtImg :src="src" :alt="alt" :width="width" :height="height" :placeholder="nPlaceholder" />
 </template>
 
 <script setup lang="ts">
-defineProps({
+// import { reactive, computed } from 'vue'
+
+const props = defineProps({
   src: {
     type: String,
     default: ''
@@ -19,6 +21,19 @@ defineProps({
   height: {
     type: [String, Number],
     default: undefined
-  }
+  },
+  placeholder: {
+    type: [Boolean, String, Number, Array],
+    default: undefined
+  },
+})
+
+// This is needed due to Markdown unable to pass array
+const nPlaceholder = computed(() => {
+  if (typeof props.placeholder !== "string") { return props.placeholder }
+
+  if (! props.placeholder.match(/\d+(,\d){1,2}/)) { return props.placeholder }
+
+  return props.placeholder.split(",").map((s) => s.trim())
 })
 </script>

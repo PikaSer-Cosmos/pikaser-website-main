83d4b0ad61e222f7ae31761ebf0180fa7ef47b37
PikachuEXE <pikachuexe@gmail.com>
2022-08-13 22:11:00 +0800

    * Prepare default meta data for I18n

--- app.vue
+++ app.vue
@@ -12,14 +12,18 @@
 </template>
 
 <script setup>
+import { useI18n } from "#i18n"
+
+const { t } = useI18n()
+
 useHead({
   titleTemplate: (titleChunk) => {
-    return titleChunk ? `${titleChunk} - PikaSer` : 'PikaSer';
+    return titleChunk ? `${titleChunk} - ${t("meta.title")}` : t("meta.title");
   },
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
   charset:  'utf-8',
   meta: [
-    { name: 'description', content: 'PikaSer - A Cosmos Validator' }
+    { name: 'description', content: t("meta.description") }
   ],
 })
 
@@ -27,3 +31,15 @@ useHead({
 const colorMode = useColorMode()
 colorMode.preference = "dark"
 </script>
+
+<i18n lang="yaml">
+en:
+  meta:
+    title: PikaSer
+    description: PikaSer - A Cosmos Validator
+
+zh:
+  meta:
+    title: PikaSer
+    description: PikaSer - A Cosmos Validator
+</i18n>

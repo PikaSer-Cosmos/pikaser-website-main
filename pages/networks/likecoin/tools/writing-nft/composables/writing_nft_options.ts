b60dd5057046ec4bbe606268c0a5b3dbedde7f63
PikachuEXE <pikachuexe@gmail.com>
2022-10-13 15:46:23 +0800

    * Update Writing NFT to add option about read NFT display style

--- pages/networks/likecoin/tools/writing-nft/composables/writing_nft_options.ts
+++ pages/networks/likecoin/tools/writing-nft/composables/writing_nft_options.ts
@@ -1,13 +1,23 @@
 import { defineStore } from "pinia"
 import { useLocalStorage } from "@vueuse/core"
 
+export enum ReadWritingNftClassDisplayStyle {
+  SAME_AS_UNREAD  = "same_as_unread",
+  COLLAPSED       = "collapsed",
+  HIDDEN          = "hidden",
+}
+
 export const useWritingNftOptionsStore = defineStore("likecoin_writing_nft_options", {
   state: (() => {
     return {
       only_writing_nft_with_complete_data_visible: useLocalStorage(
         "likecoin_writing_nft_options__only_writing_nft_with_complete_data_visible",
         false,
-      )
+      ),
+      read_writing_nft_class_display_style: useLocalStorage(
+      "likecoin_writing_nft_options__read_writing_nft_class_display_style",
+      ReadWritingNftClassDisplayStyle.SAME_AS_UNREAD,
+      ),
     }
   }),
   actions: {
